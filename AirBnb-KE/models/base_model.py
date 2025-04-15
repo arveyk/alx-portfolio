63ff0d1a80e2d53f441454e07b9edbc41d7ebdfa
Harvey W. Kis <harveykisiangani@gmail.com>
2025-04-15 13:33:58 +0300

    Object stored in json file

--- AirBnb-KE/models/base_model.py
+++ AirBnb-KE/models/base_model.py
@@ -4,17 +4,18 @@ Base_model for airbnb clone
 """
 from datetime import datetime
 import uuid
+from . import storage
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        #self.id = str(uuid.uuid4())
-        #self.created_at = datetime.now()
 
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()   
             self.updated_at = datetime.now()
+            storage.new(self)
+
         else:
             self.created_at = datetime.fromisoformat(kwargs.get("created_at"))
             self.updated_at = datetime.fromisoformat(kwargs.get("updated_at"))
@@ -25,8 +26,13 @@ class BaseModel:
 
     def save(self):
         self.update_at = str(datetime.now())
+        storage.save()
     def to_dict(self):
-        self.__dict__.get('__class__', BaseModel.__name__)
+        """Return the Dictionary representation of object
+        Args: none
+        Return: dictionary rep
+        """
+        self.__dict__['__class__']= self.__class__.__name__
 
         created_at = self.__dict__.get("created_at")
         updated_at = self.__dict__.get("updated_at")
@@ -35,5 +41,3 @@ class BaseModel:
         self.__dict__["updated_at"] = str(datetime.isoformat(updated_at))
         
         return dict(self.__dict__)
-
-
---
+++ AirBnb-KE/models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""File Storage"""
+import json
+import os
+
+
+class FileStorage:
+    """ file storage class
+    """
+    __file_path = "./file.json"
+    __objects = {}
+
+    def __init__(self):
+        """Init method
+        """
+        pass
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        obj_key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[obj_key] = obj.to_dict()
+        print(obj, obj.__str__)
+
+    def save(self):
+        """Saves , serialixes __objects
+        Args: None
+        Returns
+        """
+        with open(self.__file_path, 'w') as f:
+            json.dump(self.__objects, f)
+
+    def reload(self):
+        """Deserialize
+        """
+        if os.path.exists(self.__file_path):
+            if os.path.getsize(self.__file_path) > 0:
+                with open(self.__file_path, 'r') as f:
+                    d = json.load(f)
+                    self.__objects = d
